b6b386d954db3a6d64eda058854f574ffa5cf63c
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-30 13:42:54 -0400

    Added the ability to navigate from the main activity to the pass details activity

--- app/src/main/java/com/example/f21comp3025w4/MainActivity.kt
+++ app/src/main/java/com/example/f21comp3025w4/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.f21comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.f21comp3025w4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //configrue a variable to access the view binding
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.passInfobutton.setOnClickListener {
+            var copiedText = binding.nameEditText.text.toString()
+            if (copiedText.isNotEmpty())
+            {
+                var intent = Intent(this, PassDetailsActivity::class.java)
+                intent.putExtra("personName",copiedText)
+                startActivity(intent)
+            }
+            else
+            {
+                Toast.makeText(this, "Enter your name at the top", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }

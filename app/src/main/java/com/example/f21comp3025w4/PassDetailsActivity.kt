d5524f65b8689079d95fff01ae592f8840cf30c2
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-30 13:49:38 -0400

    Updated the ability to read from the intent

--- app/src/main/java/com/example/f21comp3025w4/PassDetailsActivity.kt
+++ app/src/main/java/com/example/f21comp3025w4/PassDetailsActivity.kt
@@ -1,12 +1,25 @@
 package com.example.f21comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.f21comp3025w4.databinding.ActivityPassDetailsBinding
 
 class PassDetailsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityPassDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pass_details)
+        binding = ActivityPassDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure the back button to return to the main activity
+        binding.backButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        //read from the intent and display the name on the activity
+        binding.welcomeTextView.text = "Welcome " + intent.getStringExtra("personName")
+
     }
 }
